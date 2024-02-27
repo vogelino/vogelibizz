da7c08b25799997062d69625df8893508ecc8d9e
Lucas Vogel <contact@vogelino.com>
2024-02-27 15:52:27 -0300

    style(AuthPage): Customize AuthPage with tailwind and CSS modules

--- src/components/auth-page/index.tsx
+++ src/components/auth-page/index.tsx
@@ -1,12 +1,24 @@
 "use client";
 import type { AuthPageProps } from "@refinedev/core";
 import { AuthPage as AuthPageBase } from "@refinedev/core";
+import { cn } from "@utility/classNames";
+import styles from "./auth-page.module.css";
 
 export const AuthPage = (props: AuthPageProps) => {
   return (
     <AuthPageBase
       {...props}
-      renderContent={(content) => <div>{content}</div>}
+      wrapperProps={{
+        className: cn("w-screen h-screen flex items-center justify-center"),
+      }}
+      contentProps={{
+        className: cn(
+          `w-full max-w-sm`,
+          "border border-gray-200 rounded-lg",
+          "shadow-xl shadow-black/5",
+          styles.form
+        ),
+      }}
     />
   );
 };
