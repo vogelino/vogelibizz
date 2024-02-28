afd1adacf0434d99a4a5274b5b0025a06e4e60a7
Lucas Vogel <contact@vogelino.com>
2024-02-28 10:11:04 -0300

    feat(breadcrumb): Style breadcrumb

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
-"use client";
+'use client'
 
-import { Suspense } from "react";
+import { Suspense } from 'react'
 
-import { Authenticated } from "@refinedev/core";
-import { NavigateToResource } from "@refinedev/nextjs-router";
+import { Authenticated } from '@refinedev/core'
+import { NavigateToResource } from '@refinedev/nextjs-router'
 
 export default function IndexPage() {
-  return (
-    <Suspense>
-      <Authenticated key="home-page">
-        <NavigateToResource />
-      </Authenticated>
-    </Suspense>
-  );
+	return (
+		<Suspense>
+			<Authenticated key="home-page">
+				<NavigateToResource resource="projects" />
+			</Authenticated>
+		</Suspense>
+	)
 }
