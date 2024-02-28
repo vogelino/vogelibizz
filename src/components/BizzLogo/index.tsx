d0fe6fbdc5310287b11effa0937123d67f9d55ea
Lucas Vogel <contact@vogelino.com>
2024-02-28 10:52:40 -0300

    refactor(BizzLogo): Improve logo using overlapping text and text-stroke

--- src/components/BizzLogo/index.tsx
+++ src/components/BizzLogo/index.tsx
@@ -4,18 +4,30 @@ import { cn } from '@utility/classNames'
 
 type BizzLogoProps = {
 	className?: string
+	textClassName?: string
 }
-function BizzLogo({ className }: BizzLogoProps) {
+function BizzLogo({ className, textClassName }: BizzLogoProps) {
 	return (
 		<span
 			className={cn(
-				'font-special text-3xl inline-block text-bg',
+				'font-special text-3xl inline-block',
 				'antialiased transition-colors',
-				styles.logo,
+				'relative w-fit',
 				className,
 			)}
 		>
-			bizz
+			<span
+				className="text-bg text-stroke-4 select-none pointer-events-none text-stroke-line"
+				aria-hidden="true"
+			>
+				Vogeli
+			</span>
+			<span className="font-sans inline-block absolute top-1.5 left-[calc(100%+0.3rem)] font-bold">
+				BIZZ
+			</span>
+			<span className={cn('absolute top-0 left-0 text-bg', textClassName)}>
+				Vogeli
+			</span>
 		</span>
 	)
 }
