d38da877a6170b88c489de6d45689f9f73f9f746
Lucas Vogel <contact@vogelino.com>
2024-05-05 22:03:08 -0400

    refactor: Redirect to login using full url

--- src/utility/data/useProjects.ts
+++ src/utility/data/useProjects.ts
@@ -7,28 +7,29 @@ import { redirect } from "next/navigation";
 
 export const projectsQueryKey = ["projects"];
 function useProjects() {
-	const { data, isPending, error } = useSuspenseQuery({
-		queryKey: projectsQueryKey,
-		queryFn: getProjects,
-	});
+  const { data, isPending, error } = useSuspenseQuery({
+    queryKey: projectsQueryKey,
+    queryFn: getProjects,
+  });
 
-	return {
-		data,
-		isPending,
-		error,
-	};
+  return {
+    data,
+    isPending,
+    error,
+  };
 }
 
 export async function getProjects() {
-	const response = await fetch(
-		`${env.client.NEXT_PUBLIC_BASE_URL}/api/projects`,
-	);
-	const json = await response.json();
+  const response = await fetch(
+    `${env.client.NEXT_PUBLIC_BASE_URL}/api/projects`
+  );
+  const json = await response.json();
 
-	if (json.error === "Unauthorized") return redirect("/login");
-	if (json.error) throw new Error(json.error);
+  if (json.error === "Unauthorized")
+    return redirect(`${env.server.NEXT_PUBLIC_BASE_URL}/login`);
+  if (json.error) throw new Error(json.error);
 
-	return json as ProjectType[];
+  return json as ProjectType[];
 }
 
 export default useProjects;
