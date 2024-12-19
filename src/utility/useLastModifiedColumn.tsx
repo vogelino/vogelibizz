ccc4e8852ff63ccc01203d8a9e99e226ab30b011
Lucas Vogel <contact@vogelino.com>
2024-12-19 09:23:56 -0300

    Add timezone to last modified

--- src/utility/useLastModifiedColumn.tsx
+++ src/utility/useLastModifiedColumn.tsx
@@ -1,32 +1,34 @@
 import type { ColumnDef } from "@tanstack/react-table";
 
 const dateFormatter = new Intl.DateTimeFormat("en-GB", {
-	year: "numeric",
-	month: "numeric",
-	day: "numeric",
-	hour: "numeric",
-	minute: "numeric",
+  year: "numeric",
+  month: "numeric",
+  day: "numeric",
+  hour: "numeric",
+  minute: "numeric",
+  timeZone: "UTC",
+  timeZoneName: "short",
 });
 
 export function useLastModifiedColumn<
-	ColumnType,
-	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  ColumnType,
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
 >(): ColumnDef<ColumnType, any> {
-	return {
-		id: "last_modified",
-		accessorKey: "last_modified",
-		size: 100,
-		header: "Last modified",
-		sortingFn: "datetime",
-		cell: function render({ getValue }) {
-			const formattedDateWithTime = dateFormatter.format(
-				new Date(getValue<string>()),
-			);
-			return (
-				<span className="text-xs font-mono text-grayDark">
-					{formattedDateWithTime}
-				</span>
-			);
-		},
-	};
+  return {
+    id: "last_modified",
+    accessorKey: "last_modified",
+    size: 100,
+    header: "Last modified",
+    sortingFn: "datetime",
+    cell: function render({ getValue }) {
+      const formattedDateWithTime = dateFormatter.format(
+        new Date(getValue<string>()),
+      );
+      return (
+        <span className="text-xs font-mono text-grayDark">
+          {formattedDateWithTime}
+        </span>
+      );
+    },
+  };
 }
