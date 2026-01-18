70de4c6e71654af127624aa5784af86bcca06a97
Lucas Vogel <lucas@deepjudge.ai>
2026-01-18 20:31:16 +0100

    ci: Add biome config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,12 +2,12 @@ import env from "@/env";
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  schema: "./src/db/schema/index.ts",
-  out: "./src/db/migrations",
-  dialect: "postgresql",
-  dbCredentials: {
-    url: env.server.POSTGRES_URL,
-  },
-  verbose: true,
-  strict: true,
+	schema: "./src/db/schema/index.ts",
+	out: "./src/db/migrations",
+	dialect: "postgresql",
+	dbCredentials: {
+		url: env.server.POSTGRES_URL,
+	},
+	verbose: true,
+	strict: true,
 });
